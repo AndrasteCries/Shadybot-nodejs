e67cbda300c5547e6e7bcd282b9846477578028e
Oleksandr Shynkarenko <1andraste1@gmail.com>
2024-06-16 13:37:05 +0300

    Add some logs

--- keep_alive.js
+++ keep_alive.js
@@ -3,6 +3,9 @@ let http = require("http");
 http
   .createServer(function (req, res) {
     res.write("I'm alive!");
+    res.write(`token = ${process.env.BOT_TOKEN}`);
+    res.write(`publicChannelId = ${process.env.CHANNEL_ID}`);
+    res.write(`ownerId = ${process.env.OWNER_ID}`);
     res.end();
   })
   .listen(8080);
